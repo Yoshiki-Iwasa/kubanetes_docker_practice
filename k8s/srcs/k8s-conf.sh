0269e89c9c6ddd0884d8bde08e806ec95f338897
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-10-02 15:17:49 +0900

    ssl 通信をするとき、k8s だとなんか弾かれる

--- k8s/srcs/k8s-conf.sh
+++ k8s/srcs/k8s-conf.sh
@@ -1,4 +1,8 @@
 #!/bin/zsh
+
+#pod を作る前に $ eval $(minikube docker-env)
+# を作る必要あり。
+
 # see what changes would be made, returns nonzero returncode if different
 kubectl get configmap kube-proxy -n kube-system -o yaml | \
 sed -e "s/strictARP: false/strictARP: true/" | \
@@ -13,3 +17,7 @@ kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manife
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
 # On first install only
 kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+#kubectl edit configmap -n kube-system kube-proxy でひらかれるconfig map のなかに
+#mode: "ipvs"
+#を追加する必要あり。
