67000d8d6b98a8b2c69f285615c685ba0bd0b22e
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-10-04 11:21:01 +0900

    wp の user と admin の設定完了

---
+++ wordpress/srcs/user.sh
@@ -0,0 +1,9 @@
+#!bin/sh
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+wp cli update
+wp core install --url=http://127.0.0.1:5050/wordpress/ --title=test --admin_user=admin --admin_password=admin --admin_email=admin@example.com  --allow-root --path=/usr/share/webapps/wordpress/
+wp user create ywake ywake@test.com  --display_name="ywake" --role=editor --user_pass="ywake" --allow-root --path=/usr/share/webapps/wordpress/
+wp user create yoshiki yoshiki@test.com  --display_name="yoshiki" --role=editor --user_pass="yoshiki" --allow-root --path=/usr/share/webapps/wordpress/
