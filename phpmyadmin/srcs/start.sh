bf3a74961c880292385273dfabf1436f2f4e1b7d
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-10-09 13:39:41 +0900

    あとは ftp　サーバーだけ

--- phpmyadmin/srcs/start.sh
+++ phpmyadmin/srcs/start.sh
@@ -1,3 +1,5 @@
 #!bin/sh
+# rc-service lighttpd restart
 rc-service telegraf start
 /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
+tail -f /dev/null
--- wordpress/srcs/start.sh
+++ wordpress/srcs/start.sh
@@ -2,5 +2,4 @@
 rc-service lighttpd restart
 rc-service telegraf start
 ./user.sh
-rc-service lighttpd stop
-/usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf
+tail -f /dev/null
