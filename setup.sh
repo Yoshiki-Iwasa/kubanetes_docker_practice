a15c42ebd13d662abe06027df4685850cf8ce2b6
Yoshiki-Iwasa <58158284+Yoshiki-Iwasa@users.noreply.github.com>
2020-10-19 02:03:43 +0900

    vm test

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-# # #!bin/zsh
-# # minikube stop
-# # minikube start
-# #wordpress のイメージ以外すべて作成する。
+# # # #!bin/zsh
+# # # minikube stop
+# # # minikube start
+# # #wordpress のイメージ以外すべて作成する。
 # eval $(minikube docker-env)
 # docker build -t nginx-test ./srcs/nginx
 # docker build -t ftps-test ./srcs/ftps
@@ -9,37 +9,37 @@
 # docker build -t inf-test ./srcs/InfluxDB
 # docker build -t mysql-test ./srcs/mysql
 # docker build -t pma-test ./srcs/phpmyadmin
-# # wordpress の イメージ作成と pod の作成は、service を作ってから。
-# #MetalLB のConfigMap を apply
+# # # wordpress の イメージ作成と pod の作成は、service を作ってから。
+# # #MetalLB のConfigMap を apply
 # kubectl apply -f ./srcs/k8s/MetalLB/metallb-config.yaml
 
-# #InfluxDB の volume と Deployment 作成
+# # #InfluxDB の volume と Deployment 作成
 
 # kubectl apply -f ./srcs/k8s/InfluxDB/inf-deployment.yaml
 
-# # #grafana の Development 作成
+# # # #grafana の Development 作成
 # kubectl apply -f ./srcs/k8s/grafana/grafana-deployment.yaml
 
-# # #nginx の deployment 作成
+# # # #nginx の deployment 作成
 # kubectl apply -f ./srcs/k8s/nginx/nginx-deployment.yaml
 
-# # # # vsftp の Deployment 作成
+# # # # # vsftp の Deployment 作成
 # kubectl apply -f ./srcs/k8s/ftps/ftps-development.yaml
 
-# # # mysql の volume と Deployment 作成
+# # # # mysql の volume と Deployment 作成
 
 # kubectl apply -f ./srcs/k8s/mysql/mysql-deployment.yaml
 
-# # # #phpmyadmin の Deployment 作成
+# # # # #phpmyadmin の Deployment 作成
 # kubectl apply -f ./srcs/k8s/phpmyadmin/pma-deployment.yaml
 
 
 
-# # # #wordpress の Service 作成
+# # # # #wordpress の Service 作成
 # kubectl apply -f ./srcs/k8s/wordpress/wp-service.yaml
 
-# # # wordpress の IP を取得して、user.sh の中身を書き換える。
-# # #wordpress の external-ip 取得
+# # # # wordpress の IP を取得して、user.sh の中身を書き換える。
+# # # #wordpress の external-ip 取得
 
 
 
@@ -58,10 +58,10 @@
 # # # # wordpress の イメージ作成
 # docker build -t wp-test ./srcs/wordpress
 
-# # # # #wordpress の Deployment 作成
+# # # # # #wordpress の Deployment 作成
 # kubectl apply -f ./srcs/k8s/wordpress/wp-deployment.yaml
 
-# # # # #以後、 External_IP のルーティング設定をしていく。
+# # # # # #以後、 External_IP のルーティング設定をしていく。
 
 # MINIKUBE_IP=$(minikube node list | awk '{print $2}')
 # NGINX_IP=$(kubectl get svc | grep nginx-service | tr -s ' ' | cut -d" " -f 4)
@@ -151,17 +151,17 @@ docker build -t wp-test ./srcs/wordpress
 #wordpress の Deployment 作成
 kubectl apply -f ./srcs/k8s/wordpress/wp-deployment.yaml
 #以後、 External_IP のルーティング設定をしていく。
-# MINIKUBE_IP=$(minikube node list | awk '{print $2}')
-# NGINX_IP=$(kubectl get svc | grep nginx-service | tr -s ' ' | cut -d" " -f 4)
-# GRA_IP=$(kubectl get svc | grep gra-service | tr -s ' ' | cut -d" " -f 4)
-# PMA_IP=$(kubectl get svc | grep pma-service | tr -s ' ' | cut -d" " -f 4)
-# FTPS_IP=$(kubectl get svc | grep ftps-service | tr -s ' ' | cut -d" " -f 4)
-# #static route を設定していく
-# echo 'user42' | sudo -S route add -host ${NGINX_IP} gw ${MINIKUBE_IP} 2>/dev/null
-# echo 'user42' | sudo -S route add -host ${GRA_IP} gw ${MINIKUBE_IP} 2>/dev/null
-# echo 'user42' | sudo -S route add -host ${PMA_IP} gw ${MINIKUBE_IP} 2>/dev/null
-# echo 'user42' | sudo -S route add -host ${WP_IP} gw ${MINIKUBE_IP} 2>/dev/null
-# echo 'user42' | sudo -S route add -host ${FTPS_IP} gw ${MINIKUBE_IP} 2>/dev/null
+# # MINIKUBE_IP=$(minikube node list | awk '{print $2}')
+# # NGINX_IP=$(kubectl get svc | grep nginx-service | tr -s ' ' | cut -d" " -f 4)
+# # GRA_IP=$(kubectl get svc | grep gra-service | tr -s ' ' | cut -d" " -f 4)
+# # PMA_IP=$(kubectl get svc | grep pma-service | tr -s ' ' | cut -d" " -f 4)
+# # FTPS_IP=$(kubectl get svc | grep ftps-service | tr -s ' ' | cut -d" " -f 4)
+# # #static route を設定していく
+# # echo 'user42' | sudo -S route add -host ${NGINX_IP} gw ${MINIKUBE_IP} 2>/dev/null
+# # echo 'user42' | sudo -S route add -host ${GRA_IP} gw ${MINIKUBE_IP} 2>/dev/null
+# # echo 'user42' | sudo -S route add -host ${PMA_IP} gw ${MINIKUBE_IP} 2>/dev/null
+# # echo 'user42' | sudo -S route add -host ${WP_IP} gw ${MINIKUBE_IP} 2>/dev/null
+# # echo 'user42' | sudo -S route add -host ${FTPS_IP} gw ${MINIKUBE_IP} 2>/dev/null
 touch ~/.lftprc
 echo "set ftp:ssl-auth TLS" >> ~/.lftprc
 echo "set ftp:ssl-force true" >> ~/.lftprc
